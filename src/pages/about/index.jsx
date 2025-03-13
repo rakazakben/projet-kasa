5e8c0eaf6e4c99e59ec9f59e0dc439b38939ce5e
rakazak <rakazakben@gmail.com>
2025-03-13 13:26:47 +0100

    page about

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,10 +1,20 @@
 import Banner from "../../components/banniere/Banner"
 import "../../styles/banner.scss"
 import bannerImage from "../../assets/banniereAbout.jpg"
+import Collapse from "../../components/description/Collapse"
+import about from "../../data/aboutText.json"
+import "../../styles/about.scss"
+
 function About() {
     return (
       <div className="body">
         <Banner texte="" img={bannerImage} isDark = {true}/>
+        <div className="section-about">
+            <Collapse titre="Fiabilité" contenu={about.fiabilite}/>
+            <Collapse titre="Respect" contenu={about.respect}/>
+            <Collapse titre="Service" contenu={about.service}/>
+            <Collapse titre="Sécurité" contenu={about.securite}/>
+        </div>
       </div>
     )
   }
